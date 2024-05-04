230cd4f715eedf641528cfec766d01fd0f8ba9b4
Hadeer Mahmoud <hadeermahmoud952013@gmail.com>
2024-05-05 01:38:19 +0300

    Offline Page Text

--- src/pages/OfflinePage/OfflinePage.tsx
+++ src/pages/OfflinePage/OfflinePage.tsx
@@ -1,9 +1,12 @@
-import offlineImg from "../../assets/offline-img.png";
-
 function OfflinePage() {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      <img src={offlineImg} alt="you're offline" className="h-full" />
+      <div className="text-center">
+        <p className="text-2xl text-[red] font-bold mb-5">You're Offline!</p>
+        <p className="text-black-700 font-semibold">
+          Please Check Your Internet Connection and Try Again
+        </p>
+      </div>
     </div>
   );
 }
