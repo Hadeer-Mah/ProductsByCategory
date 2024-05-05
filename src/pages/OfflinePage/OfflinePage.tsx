452c932156b1d7a7ebb56193209896765a4701f7
Hadeer Mahmoud <hadeermahmoud952013@gmail.com>
2024-05-05 17:34:08 +0300

    Offline Page Translation

--- src/pages/OfflinePage/OfflinePage.tsx
+++ src/pages/OfflinePage/OfflinePage.tsx
@@ -1,10 +1,13 @@
+import { useTranslation } from "react-i18next";
+
 function OfflinePage() {
+  const { t } = useTranslation();
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="text-center">
-        <p className="text-2xl text-[red] font-bold mb-5">You're Offline!</p>
+        <p className="text-2xl text-[red] font-bold mb-5">{t("You're Offline!")}</p>
         <p className="text-black-700 font-semibold">
-          Please Check Your Internet Connection and Try Again
+          {t("Please Check Your Internet Connection and Try Again")}
         </p>
       </div>
     </div>
